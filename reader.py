0e9c940ccc630435a7133077829787400d12042d
makometr <makometr@yandex.ru>
2019-11-08 04:30:45 +0300

    structure ended. reading array from csv in plans:

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
 import rosbag
 import rosmsg
+import csv
 from operator import itemgetter
+import json
 
 def makeTypes(lines):
     types = []
     names = []
+    real_names = []
 
     for line in lines:
         curSpaces = len(line) - len(line.lstrip())
@@ -19,7 +22,7 @@ def makeTypes(lines):
     types = mergeLines(types)
     types = [elem[1] for elem in types if elem[2]]
 
-    return (types, names)
+    return (types, names, real_names)
 
 def mergeLines(lines):
     minIndent = min(lines, key=itemgetter(0))[0]
@@ -60,16 +63,36 @@ def getBagData(bag):
         storage["msgs_list"] = []
         try:
             lines = rosmsg.get_msg_text(value[0]).splitlines()
-            msgTypes, msgNames = makeTypes(lines)
-            print(msgTypes)
-            print(msgNames)
-        except rosmsg.ROSMsgException as identifier:
+            msgTypes, msgNames, real_names = makeTypes(lines)
+            for type in msgTypes:
+                print type
+            for name in msgNames:
+                print name
+        except rosmsg.ROSMsgException:
             print 'UNKNOWN'
+        
+        for i in range(len(msgNames)):
+            storage["msgs_list"].append({"msg_name" : msgNames[i], "msg_type" : msgTypes[i], "msgs" : []})
         # for topic, msg, t in bag.read_messages(topics=[key]):
+        #     print str(msg)
+        #     break
 
         data.append(storage)
     return data
 
-bag = rosbag.Bag('bags/2011-01-24-06-18-27.bag')
-getBagData(bag)
+# bag = rosbag.Bag('bags/2011-01-24-06-18-27.bag')
+bag = rosbag.Bag('bags/square.bag')
+data = getBagData(bag)
+print json.dumps(data, indent=2)
 bag.close()
+
+# csv_path = "bags/square/_slash_turtle1_slash_cmd_vel.csv"
+# with open(csv_path, "r") as f_obj:
+#     reader = csv.reader(f_obj)
+#     names = reader.next()
+#     print names
+
+    
+#     for row in reader:
+#         content = list(row[i] for i in [1,2])
+#         print content
