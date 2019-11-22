b6f050014dd0bf9045f30a24ba067e984a1616d2
makometr <makometr@yandex.ru>
2019-11-22 22:40:58 +0300

    Changes in dbQuery class logic

--- dbQueryManager.py
+++ dbQueryManager.py
@@ -1,23 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-s
 from pymongo import MongoClient
 
 
 class dbQueryManager:
-    def __init__(self, collection_name):
+    '''
+    В конструктор передаётся название коллекции и базы данных (опционально).
+    '''
+    def __init__(self, db_name="test_database"):
         self.client = MongoClient()
-        self.db = self.client["test_database"]
-        self.collection = self.db[collection_name]
+        self.db = self.client[db_name]
 
-    def getNumberOfDocuments(self):
-        # result = self.collection.count({})
+    def getNumberOfDocuments(self, collection_name):
+        # collection = self.db[collection_name]
+        # result = collection.count({})
         result = 3
         return result
 
-    def getDocumentNames(self):
+    def getDocumentNames(self, collection_name):
         result = ["Name1", "Name2", "Name3"]
         return result
 
-    def getMainInfo(self):
-        # queryResult = self.collection.find({}, {"topics_list.msgs_list.msgs" : {"$slice": 10}})
+    def getMainInfo(self, collection_name):
+        # collection = self.db[collection_name]
+        # queryResult = collection.find({}, {"topics_list.msgs_list.msgs" : {"$slice": 10}})
         # for fileBag in queryResult:
         #     del fileBag["_id"]
         queryResult = {u'duration': 0.599675178527832, u'topics_list': [{u'msgs_type': u'geometry_msgs/Quaternion', u'msgs_list': [{u'msg_name': u'x', u'msg_type': u'float64', u'msgs': [u'10.0', u'10.0', u'10.0', u'10.0']}, {u'msg_name': u'y', u'msg_type': u'float64', u'msgs': [u'50.0', u'33.3333333333', u'25.0', u'20.0']}, {u'msg_name': u'z', u'msg_type': u'float64', u'msgs': [u'11.0', u'12.0', u'13.0', u'14.0']}, {u'msg_name': u'w', u'msg_type': u'float64', u'msgs': [u'22.2', u'22.2', u'22.2', u'22.2']}], u'topic_name': u'quaternionTopic', u'msgs_num': 4}, {u'msgs_type': u'geometry_msgs/Pose', u'msgs_list': [{u'msg_name': u'position/x', u'msg_type': u'geometry_msgs/Point/float64', u'msgs': [u'1.1', u'1.1', u'1.1', u'1.1']}, {u'msg_name': u'position/y', u'msg_type': u'geometry_msgs/Point/float64', u'msgs': [u'2.2', u'2.2', u'2.2', u'2.2']}, {u'msg_name': u'position/z', u'msg_type': u'geometry_msgs/Point/float64', u'msgs': [u'3.3', u'3.3', u'3.3', u'3.3']}, {u'msg_name': u'orientation/x', u'msg_type': u'geometry_msgs/Quaternion/float64', u'msgs': [u'11.1', u'12.1', u'13.1', u'14.1']}, {u'msg_name': u'orientation/y', u'msg_type': u'geometry_msgs/Quaternion/float64', u'msgs': [u'13.1', u'14.1', u'15.1', u'16.1']}, {u'msg_name': u'orientation/z', u'msg_type': u'geometry_msgs/Quaternion/float64', u'msgs': [u'24.1', u'25.1', u'26.1', u'27.1']}, {u'msg_name': u'orientation/w', u'msg_type': u'geometry_msgs/Quaternion/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0']}], u'topic_name': u'poseTopic', u'msgs_num': 4}], u'date_creation': u'18-11-2019 04:46:28', u'filename': u'bags/Double.bag'}, {u'duration': 10.599474668502808, u'topics_list': [{u'msgs_type': u'std_msgs/String', u'msgs_list': [{u'msg_name': u'data', u'msg_type': u'string', u'msgs': [u'"hello world 1573044926.85"', u'"hello world 1573044926.95"', u'"hello world 1573044927.05"', u'"hello world 1573044927.15"', u'"hello world 1573044927.25"', u'"hello world 1573044927.35"', u'"hello world 1573044927.45"', u'"hello world 1573044927.55"', u'"hello world 1573044927.65"', u'"hello world 1573044927.75"']}], u'topic_name': u'/chatter', u'msgs_num': 107}], u'date_creation': u'21-11-2019 23:18:53', u'filename': u'bags/hello.bag'}, {u'duration': 29.82285976409912, u'topics_list': [{u'msgs_type': u'geometry_msgs/Twist', u'msgs_list': [{u'msg_name': u'linear/x', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'2.0', u'2.0', u'2.0', u'2.0', u'2.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0']}, {u'msg_name': u'linear/y', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0']}, {u'msg_name': u'linear/z', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0']}, {u'msg_name': u'angular/x', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0']}, {u'msg_name': u'angular/y', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'0.0']}, {u'msg_name': u'angular/z', u'msg_type': u'geometry_msgs/Vector3/float64', u'msgs': [u'0.0', u'0.0', u'0.0', u'0.0', u'0.0', u'-2.0', u'-2.0', u'-2.0', u'-2.0', u'-2.0']}], u'topic_name': u'/turtle1/cmd_vel', u'msgs_num': 325}], u'date_creation': u'21-11-2019 23:19:16', u'filename': u'bags/square.bag'}
@@ -25,10 +31,14 @@ class dbQueryManager:
 
 
 
+
 if __name__ == "__main__":
-    manager = dbQueryManager("bagfiles_test")
-    manager.getNumberOfDocuments()
+    manager = dbQueryManager()
+    collection = "bagfiles_test"
     
-    listOfBags = manager.getMainInfo()
-    print(listOfBags)
+    manager.getNumberOfDocuments(collection)
+    manager.getDocumentNames(collection)
+    
+    listOfBags = manager.getMainInfo(collection)
+    # print(listOfBags)
 
--- server.py
+++ server.py
@@ -1,21 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-s
 from flask import Flask, render_template, jsonify, make_response
 from pymongo import MongoClient
 from adapter import getDataFromBag
 from dbQueryManager import dbQueryManager
-# from pprint import pprint
-# client = MongoClient()
-queryManager = dbQueryManager("bagfiles_test")
-app = Flask(__name__)
+from pprint import pprint
 
-# print(getDataFromBag('bags/Double.bag'))
+DB = dbQueryManager("bagfiles_test")
+app = Flask(__name__)
+defaultCollection = "bagfiles_test"
 
 @app.route("/")
 def hello(name=None):
     return render_template('index.html', name=name)
 
-@app.route("/getDocunentsNumber")
+@app.route("/getDocumentsNumber")
 def getDocsData():
-    result = queryManager.getNumberOfDocuments()
+    result = DB.getNumberOfDocuments(defaultCollection)
 
     ans = {'status': True, 'result': result}
     print(jsonify(ans))
@@ -24,64 +25,10 @@ def getDocsData():
 
 @app.route('/addData', methods=['POST'])
 def addData():
-    # db = client.test_database
-    # topic = "quaternionTopic"
-    # msgName = "x"
-    # summary = db.bagfiles_test.aggregate([
-    #  { "$match": { 
-    #     "$and": [
-    #          { "filename": "bags/Double.bag" },
-    #          { "topics_list.topic_name": topic },
-    #          { "topics_list.msgs_list.msg_name": msgName }
-    #     ]
-    #  } }, # Match documents to shrink their quantity
-    #  { "$project": {
-    #     #  "list": "$topics_list.msgs_list.msgs",
-    #      "filteredByTopic": {
-    #          "$filter": {
-    #             "input": "$topics_list",
-    #             "cond": { "$and": [
-    #                 { "$eq": [ '$$this.topic_name', topic ] },
-    #              ] }
-    #          }
-    #      }
-    #  } }, # Filter topics_list by topic name
-    #  { "$project": {
-    #      "filteredByTopic": {"$arrayElemAt": ["$filteredByTopic", 0] },
-    #  } }, # Take first elem in result
-    #  { "$project": {
-    #      "filteredByTopic": "$filteredByTopic.msgs_list",
-    #  } }, # Take msges in that topic
-    #  { "$project": {
-    #      "filteredByMsgName": {
-    #          "$filter": {
-    #              "input": "$filteredByTopic",
-    #              "cond": {
-    #                  "$eq": [ "$$this.msg_name", msgName]
-    #              }
-    #          }
-    #      }
-    #  } }, # Filter msges by msg name
-    #  { "$project": {
-    #      "filteredByMsgName": {"$arrayElemAt": ["$filteredByMsgName", 0] },
-    #  } }, # Take first elem in result
-    # #  { "$project": {
-    # #      "sum": { "$sum": "$filteredByMsgName.msgs" },
-    # #  } }, # Sum all elements in msgs
-    # ]) # .next()
-    # # pprint(list(summary));
-    # numbersList = list(summary)[0]['filteredByMsgName']['msgs']
-    # firstEl = numbersList[0]
-    # print("List elem type:", type(firstEl))
-    # print("Array:")
-    # print(numbersList)
-    # numbersList = map(float, numbersList)
-    # print("Sum:", sum(numbersList))
-    db = client.test_database
-    newDocument = getDataFromBag('bags/square.bag')
-    bags = db.bagfiles_test
-    post_id = bags.insert_one(newDocument).inserted_id
-    print(post_id)
+    # newDocument = getDataFromBag('bags/square.bag')
+    # bags = db.bagfiles_test
+    # post_id = bags.insert_one(newDocument).inserted_id
+    # print(post_id)
 
     ans = {'status': True}
     print(jsonify(ans))
